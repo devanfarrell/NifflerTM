80fcc947fecf983486e1de300012617855a8f58d
Devan <devcfar@yahoo.com>
2017-04-12 17:31:11 -0700

    Initial Commit

---
+++ NifflerTM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NifflerTM
+//
+//  Created by Devan Farrell on 4/12/17.
+//  Copyright © 2017 Devan Farrell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
