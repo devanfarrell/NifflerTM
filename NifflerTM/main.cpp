80ef25b58be31d2c404321388e0fe7040aa6fbac
Devan <devcfar@yahoo.com>
2017-04-30 17:16:32 -0700

    Merge branch 'iMac' into MergeIntermediate

--- NifflerTM/main.cpp
+++ NifflerTM/main.cpp
@@ -1,16 +1,25 @@
 #include "TM.hpp"
 
 #include <iostream>
-#define FILE "/Users/devan/NifflerTM/NifflerTM/tm"
 
 int main(int argc, const char * argv[]) {
   TM tm;
-  bool openAndValid = tm.load(FILE);
   
-  if(openAndValid)
+  if(argc > 1)
   {
-    std::cout << "Successfully loaded!" << std::endl;
-    tm.initiate();
+    bool openAndValid = tm.load(argv[1]);
+    
+    if(openAndValid)
+    {
+      std::cout << "Successfully loaded!" << std::endl;
+      tm.initiate();
+    }
+    
+  }
+  else
+  {
+    fprintf(stderr, "Must pass an argument!\n");
+    exit(1);
   }
     return 0;
 }
